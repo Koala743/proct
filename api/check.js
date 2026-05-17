b9b8abf197b944e4ae79ca4cb2745edecd42e561
Koala743 <armijosfeo5@gmail.com>
2026-05-17 02:35:17 -0500

    Actualizar check.js

--- api/check.js
+++ api/check.js
@@ -1,9 +1,13 @@
 global.purchasedEmails = global.purchasedEmails || {};
 
 module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
+
   const { email } = req.query;
   if (!email) return res.status(400).json({ error: "Email required" });
 
-  const found = !!global.purchasedEmails[email];
-  res.status(200).json({ purchased: found });
+  res.status(200).json({ purchased: !!global.purchasedEmails[email] });
 };
