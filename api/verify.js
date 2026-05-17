6dd36fe187012680069c77d34f890ce1d9e31413
Koala743 <armijosfeo5@gmail.com>
2026-05-17 02:35:00 -0500

    Actualizar verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,11 +1,13 @@
 global.codes = global.codes || {};
 
 module.exports = async function handler(req, res) {
-  const { email, code } = req.query;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
 
-  if (global.codes[email] === code) {
-    return res.status(200).json({ success: true });
-  }
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  const { email, code } = req.query;
+  if (global.codes[email] === code) return res.status(200).json({ success: true });
 
   res.status(400).json({ success: false });
 };
