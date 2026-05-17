cd386c4509d99c56d3a7cc699c0ef3dd2721a164
Koala743 <armijosfeo5@gmail.com>
2026-05-17 02:34:48 -0500

    Actualizar send.js

--- api/send.js
+++ api/send.js
@@ -1,18 +1,17 @@
 const { Resend } = require("resend");
-
 const resend = new Resend(process.env.RESEND_API_KEY);
-
 global.codes = global.codes || {};
 
 module.exports = async function handler(req, res) {
-  const email = req.query.email;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
 
-  if (!email) {
-    return res.status(400).json({ error: "Email required" });
-  }
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  const email = req.query.email;
+  if (!email) return res.status(400).json({ error: "Email required" });
 
   const code = Math.floor(100000 + Math.random() * 900000).toString();
-
   global.codes[email] = code;
 
   await resend.emails.send({
